338a82fd823e26395818429ab33895862e7cb4ad
AlvaroCaffaro <105836282+AlvaroCaffaro@users.noreply.github.com>
2024-10-06 07:56:02 -0300

    -

--- src/connection.ts
+++ src/connection.ts
@@ -2,16 +2,14 @@ import mysql, { Connection } from 'mysql2/promise';
 
 // Función para crear una conexión a la base de datos
 export const createConnection = async (): Promise<Connection> => {
-    try {
+  
         const connection = await mysql.createConnection({
             host: 'localhost',     
-            user: 'tu_usuario',  
-            password: 'tu_contraseña', 
+            user: process.env.DATABASEUSER,  
+            password: process.env.DATABASEPASSWORD, 
             database: 'tu_base_de_datos'
         });
 
         return connection;
-    } catch (error) {
-        throw error;
-    }
+    
 };
